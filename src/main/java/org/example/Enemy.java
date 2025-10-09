00143a925e92fae14426183df4b44230e0bc0e81
MkMekhontsev <mkmekhontsev@gmail.com>
2025-10-09 11:45:30 +0300

    добавлен move

---
+++ src/main/java/org/example/Enemy.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Enemy extends Unit {
+    public Enemy(int x, int y){
+        super(x, y);
+    }
+    @Override
+    public void move(int newX, int newY, Map map) {
+        super.move(newX, newY, map);
+    }
+}
---
+++ src/main/java/org/example/Hero.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Hero extends Unit{
+    public Hero(int x, int y){
+        super(x,y);
+    }
+}
